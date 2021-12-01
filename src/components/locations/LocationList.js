86c897fe9a4c37c8627a9563fbd7f255f708e4a9
jmason87 <joseph.mason87@gmail.com>
2021-12-01 10:06:58 -0600

    added a button to locationlist.js

--- src/components/locations/LocationList.js
+++ src/components/locations/LocationList.js
@@ -1,19 +1,32 @@
 import React, { useEffect, useState } from "react"
+import useSimpleAuth from "../../hooks/ui/useSimpleAuth";
 import LocationRepository from "../../repositories/LocationRepository";
 import Location from "./Location"
 import "./LocationList.css"
 
 
 export const LocationList = () => {
-    const [ locations, updateLocations ] = useState([])
+    const [locations, updateLocations] = useState([])
+    const { getCurrentUser } = useSimpleAuth()
 
     useEffect(() => {
         LocationRepository.getAll().then(updateLocations)
     }, [])
 
     return (
-        <div className="locations">
-            {locations.map(l => <Location key={l.id} location={l} />)}
-        </div>
+        <>
+            {
+                getCurrentUser().employee
+                    ? <div className="centerChildren btn--newResource">
+                        <button className="btn btn-success "
+                        >Add Treatment</button>
+                    </div>
+                    : "effaf"
+            }
+
+            <div className="locations">
+                {locations.map(l => <Location key={l.id} location={l} />)}
+            </div>
+        </>
     )
 }
