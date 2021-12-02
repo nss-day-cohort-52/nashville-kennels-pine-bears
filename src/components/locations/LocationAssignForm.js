d892b99f08415836d9d934b0922802836fcd1d18
jmason87 <joseph.mason87@gmail.com>
2021-12-02 09:57:13 -0600

    history.push not working

--- src/components/locations/LocationAssignForm.js
+++ src/components/locations/LocationAssignForm.js
@@ -39,9 +39,10 @@ export const LocationAssignForm = () => {
             }
         
             EmployeeLocationRepository.addNewEmployeeLocation(empLocation)
-                .then(() => {history.push("/locations")})
+                
                 }
             }
+
 
     return(
         <form className="animalForm">
@@ -76,16 +77,17 @@ export const LocationAssignForm = () => {
             </select>
         </div>
         <button type="submit"
-            onClick={(evt)=>{
-                EmployeeLocationRepository.checkCurrentAssignment(empId, locationId).then((data)=>{
+            onClick=
+            {(evt)=> { EmployeeLocationRepository.checkCurrentAssignment(empId, locationId).then((data)=>{
                     if (data.length > 0){
                         window.alert("Employee is already assigned to this location")
                     } else {
                         constructNewEmployeeLocation(evt)
+                        history.push("/locations")
                     }
                 })
             }}
-            //onClick={constructNewEmployeeLocation}
+            // onClick={constructNewEmployeeLocation}
             className="btn btn-primary"> Assign </button>
     </form>
     )
